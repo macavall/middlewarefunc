234055ed9c9ea3d88fdbf2475009c4cbb7363d02
Matt <macavall@microsoft.com>
2024-08-20 19:05:52 -0500

    first

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Azure.Functions.Worker.Middleware;
@@ -7,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
@@ -26,6 +30,7 @@ internal class Program
         {
             services.AddApplicationInsightsTelemetryWorkerService();
             services.ConfigureFunctionsApplicationInsights();
+            services.AddSingleton<ITelemetryInitializer, CustomTelemetryInitializer>();
         })
         .ConfigureLogging(logging =>
         {
@@ -34,6 +39,63 @@ internal class Program
         .Build();
 
         await host.RunAsync();
+    }
+}
+
+public class CustomTelemetryInitializer : ITelemetryInitializer
+{
+    public void Initialize(ITelemetry telemetry)
+    {
+        if (telemetry is RequestTelemetry requestTelemetry)
+        {
+            // Example: Modify request telemetry
+            AlterTelemetry(requestTelemetry);
+        }
+        else if (telemetry is DependencyTelemetry dependencyTelemetry)
+        {
+            // Example: Modify dependency telemetry (outgoing REST requests)
+            AlterTelemetry(dependencyTelemetry);
+        }
+    }
+
+    private void AlterTelemetry(object telemetry)
+    {
+        // Use reflection to alter the telemetry properties
+        var type = telemetry.GetType();
+
+        // Modify properties using reflection, for example:
+        var urlProperty = type.GetProperty("Url");
+        if (urlProperty != null)
+        {
+            var urlValue = urlProperty.GetValue(telemetry) as Uri;
+            if (urlValue != null)
+            {
+                // Modify the URL or other properties
+                urlProperty.SetValue(telemetry, new Uri(urlValue.ToString().Replace("http:", "https:")));
+            }
+        }
+
+        // Modify other properties as needed
+        var responseCodeProperty = type.GetProperty("ResponseCode");
+        if (responseCodeProperty != null)
+        {
+            var responseCodeValue = responseCodeProperty.GetValue(telemetry) as string;
+            if (!string.IsNullOrEmpty(responseCodeValue) && responseCodeValue == "200")
+            {
+                responseCodeProperty.SetValue(telemetry, "201");
+            }
+        }
+
+        // Add custom properties
+        var customDimensionsProperty = type.GetProperty("Properties");
+        if (customDimensionsProperty != null)
+        {
+            var properties = customDimensionsProperty.GetValue(telemetry) as IDictionary<string, string>;
+            if (properties != null)
+            {
+                properties["CustomProperty"] = "CustomValue";
+            }
+        }
     }
 }
 
